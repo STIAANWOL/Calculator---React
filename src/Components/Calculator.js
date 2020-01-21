a8a0272a0ed2c412694d362ce41f2a9988821ac2
Stiaan Wolfaardt <stiaan99@gmail.com>
2020-01-21 07:09:43 +0200

    Clear input after operator is clicked

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -16,35 +16,59 @@ class Calculator extends Component {
   }
 
 clickHandler(value) {
-  this.setState({
-    currentInput: this.state.currentInput + value
-  })
-  // switch(value) {
-  //   case "/":
-  //     this.setState = {
-  //       operator: "/"
-  //     }
-  //   case "x":
-  //     this.setState = {
-  //       operator: "x"
-  //     }
-  //   case "-":
-  //     this.setState = {
-  //       operator: "-"
-  //     }
-  //   case "+":
-  //     this.setState = {
-  //       operator: "+"
-  //     }
-  //   case "=":
-  //     this.setState = {
-  //       operator: "="
-  //     }
-  //   default:
-  //     if (this.state.operator = null) {
-  //       console.log("testing")
-  //     }
-  // }
+  switch(value) {
+    case "/":
+      this.setState ({
+        operator: "/",
+        firstVal: this.state.currentInput,
+        currentInput: ""
+      })
+      break;
+    case "x":
+      this.setState({
+        operator: "x",
+        firstVal: this.state.currentInput,
+        currentInput: ""
+      })
+      break;
+    case "-":
+      this.setState({
+        operator: "-",
+        firstVal: this.state.currentInput,
+        currentInput: ""
+      })
+      break;
+    case "+":
+      this.setState({
+        operator: "+",
+        firstVal: this.state.currentInput,
+        currentInput: ""
+      })
+      break;
+    case "=":
+      this.setState({
+        operator: "=",
+        firstVal: this.state.currentInput,
+        currentInput: ""
+      })
+      if (this.state.firstVal !== "") {
+        this.setState({
+          secondVal: this.state.currentInput
+        })
+        console.log("First val is not empty")
+        let calculation = this.state.firstVal + this.state.operator + this.state.secondVal + "="
+
+        console.log(calculation)
+        console.log(this.state.answer)
+      }
+      break;
+    default:
+    console.log(this.state.firstVal)
+      this.setState({
+        currentInput: this.state.currentInput + value
+      })
+      console.log("Current input", this.state.currentInput)
+  }
 }
 
 changeHandler() {
